7348d600bb69ca3f99a704766f05573cef07831f
Kevin Holmes <kevinholmes921@gmail.com>
2022-04-08 17:12:40 +0000

    updating hello.js

--- hello.js
+++ hello.js
@@ -9,3 +9,19 @@ const sayHello  = function (name) {
 }
 
 sayHello("Caliban");
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
